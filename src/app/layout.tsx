42504f33fb09058fd4a4380b9f066b5c4ef0db3e
Arsalan <uddinarsalan91@gmail.com>
2025-02-12 02:50:13 +0530

    fix eslint issue,added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import QueryProvider from "@/lib/QueryProvider";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics />
         <QueryProvider> {children}</QueryProvider>
       </body>
     </html>
--- src/components/sidebar-left.tsx
+++ src/components/sidebar-left.tsx
@@ -7,7 +7,6 @@ import {
   CheckSquare,
   Calendar,
   BarChart,
-  Settings,
   LogOut,
   Star,
   Clock,
